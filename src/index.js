2a3f9216a07271109e5733c69fd8305de6a01290
Lucas H <lhoskin@charlotte.edu>
2025-11-18 23:54:29 -0500

    update issue

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Paths from "./components/Paths.js";
 
-// RESET ONLY margin/padding
-document.body.style.margin = "0";
-document.documentElement.style.margin = "0";
-document.body.style.padding = "0";
-document.documentElement.style.padding = "0";
+// // RESET ONLY margin/padding
+// document.body.style.margin = "0";
+// document.documentElement.style.margin = "0";
+// document.body.style.padding = "0";
+// document.documentElement.style.padding = "0";
 
 // ❌ do NOT set overflow hidden anymore
 // document.body.style.overflow = "hidden";
