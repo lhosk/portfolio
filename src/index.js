fcd380ec3ee20b645479abcbd311c799e1030b35
Lucas H <lhoskin@charlotte.edu>
2025-11-18 23:29:38 -0500

    udpate paths

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Paths from "./components/Paths.js";
 
-document.body.style.background = "black";
-document.documentElement.style.background = "black";
-document.body.style.margin = "0";
-document.documentElement.style.margin = "0";
-
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
