3b30d64ac67e46e49d57d5c7d5634359ed58b39b
Lucas H <lhoskin@charlotte.edu>
2025-11-18 23:57:26 -0500

    update all and checking

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Paths from "./components/Paths.js";
 
-// // RESET ONLY margin/padding
-// document.body.style.margin = "0";
-// document.documentElement.style.margin = "0";
-// document.body.style.padding = "0";
-// document.documentElement.style.padding = "0";
-
-// ❌ do NOT set overflow hidden anymore
-// document.body.style.overflow = "hidden";
-// document.documentElement.style.overflow = "hidden";
+document.body.style.margin = "0";
+document.documentElement.style.margin = "0";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter basename="/portfolio">
+  <HashRouter>
     <Paths />
-  </BrowserRouter>
+  </HashRouter>
 );
